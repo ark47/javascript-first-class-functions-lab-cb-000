8d6b90cc45da14a64de16a657f30cbe9b9e7443d
Chris Nunez <cenunez47@gmail.com>
2018-04-08 00:24:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-// function countdown(callback) {
-//   setTimeout(callback, 2000); //setTimeout() waits a designated amount of time before performing the first argument.
-// }
-//
-// var doubler = createMultiplier(2)
-// var tripler = createMultiplier(3)
-// //var quadrupeler = createMultiplier(4)
-//
-// function createMultiplier(multiplierValue) {
-//   return function (x) {
-//     return multiplierValue * x;
-//   }
-// }
-//
-// function multiplier(y, z) {
-//   return y * z;
-// }
-//
-// var doublerWithBind = multiplier.bind(null, 2)
-// var triplerWithBind = multiplier.bind(null, 3)
-
 function countdown(callback) {
   setTimeout(callback, 2000);
 }
@@ -31,3 +10,6 @@ function createMultiplier(multiplierValue) {
 
 var doubler = createMultiplier(2);
 var tripler = createMultiplier(3);
+
+doublerWithBind = createMultiplier.bind(null, 2);
+triplerWithBind = createMultiplier.bind(null, 3);
